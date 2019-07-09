72bf2f261e170374a0d1005639847ce945773ce1
Cameron Edwards <edwards.cameron.pc@gmail.com>
2019-07-09 12:38:28 -0600

    add support for omnibox (keyword "hunt")

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,1 +1,7 @@
-// todo
+chrome.omnibox.onInputEntered.addListener(text => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    if (tabs.length) {
+      chrome.tabs.sendMessage(tabs[0].id, text)
+    }
+  })
+})
