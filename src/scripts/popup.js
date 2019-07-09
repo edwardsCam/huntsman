a40306a94f018245661126fda46879a43b7ff593
Cameron Edwards <edwards.cameron.pc@gmail.com>
2019-07-09 12:22:10 -0600

    refactor, use message-passing architecture instead of local storage

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,12 +1,7 @@
 const clickHandler = e => {
   const value = document.getElementById('searchInput').value.replace(/\"/g, '\\"')
-
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    const { id } = tabs[0]
-    const code = `chrome.storage.local.set({ _HUNTSMAN_searchQuery: "${value}" })`
-    chrome.tabs.executeScript(id, { code }, () => {
-      chrome.tabs.executeScript(id, { file: 'src/scripts/searchForElements.js' })
-    })
+    chrome.tabs.sendMessage(tabs[0].id, value)
   })
 }
 
---
+++ src/scripts/searchAndHighlight.js
@@ -0,0 +1,43 @@
+const searchAndHighlight = query => {
+  clear()
+  const sanitizedQuery = sanitize(query)
+  if (sanitizedQuery) {
+    document.querySelectorAll(sanitizedQuery).forEach(drawOutline)
+  }
+}
+
+const clear = () => document.querySelectorAll('[huntsman-target]').forEach(el => el.remove())
+
+const drawOutline = element => {
+  document.body.appendChild(
+    createOutline(
+      element.getBoundingClientRect()
+    )
+  )
+}
+
+const createOutline = rect => {
+  const outline = document.createElement('div')
+  outline.setAttribute('huntsman-target', '')
+  outline.style.position = 'absolute'
+  outline.style.border = '2px solid red'
+  outline.style.overflow = 'hidden'
+  outline.style.background = 'none'
+  outline.style.top = `${rect.top}px`
+  outline.style.left = `${rect.left}px`
+  outline.style.width = `${rect.width}px`
+  outline.style.height = `${rect.height}px`
+  return outline
+}
+
+// TODO do we need to sanitize input to querySelectorAll?
+// something like "h1 > div" is a valid css selector, we don't want to escape &gt
+const sanitize = unsafeStr => unsafeStr
+// unsafeStr
+//   .replace(/&/g, '&amp;')
+//   .replace(/</g, '&lt;')
+//   .replace(/>/g, '&gt;')
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+  searchAndHighlight(request)
+})
